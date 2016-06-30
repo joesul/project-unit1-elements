ae131a785c7c3ca3ee1174650712aeee99ca1d06
joseph sul <joesul7@gmail.com>
2016-06-30 00:05:47 -0400

    finished outline of project

--- main.js
+++ main.js
@@ -1,85 +1,124 @@
 console.log("main.js has loaded...");
+document.addEventListener("DOMContentLoaded", start());
 
-var p1Element;
-var p2Element;
+//global variables
+var p1Choice = "";
+var p2Choice = "";
+var p1HP = 3;
+var p2HP = 3;
 
-//player 1 selection
-function p1Chosen() {
-  window.addEventListener("keydown", function(e) {
-    if (e.keyCode == 81) {
-      p1Element = "fire";
-      console.log("Player 1 has chosen FIRE!");
-      console.log(p1Element);
-    }
-    else if (e.keyCode == 87) {
-      p1Element = "water";
-      console.log("Player 1 has chosen WATER!");
-    }
-    else if (e.keyCode == 69) {
-      p1Element = "earth";
-      console.log("Player 1 has chosen EARTH!");
-    }
-    // else {
-    //   alert("Player 1 please choose your element!")
-    // }
-  })
-  return p1Element;
-}
-console.log(p1Element);
-
-//player 2 selection
-function p2Chosen() {
-  window.addEventListener("keydown", function(e) {
-    if (e.keyCode == 73) {
-      p2Element = "fire";
-      console.log("Player 2 has chosen FIRE!");
-    }
-    else if (e.keyCode == 79) {
-      p2Element = "water";
-      console.log("Player 2 has chosen WATER!");
-    }
-    else if (e.keyCode == 80) {
-      p2Element = "earth";
-      console.log("Player 2 has chosen EARTH!");
-    }
-    // else {
-    //   alert("Player 2 please choose your element!")
-    // }
-  } )
-  return p2Element;
+//starts game
+function start () {
+  selection();
 }
 
-var winner = function(choice1,choice2) {
-
-  if (choice1 === choice2){
-    alert ("The result is a tie!");
-  }
-  else if (choice1 === "fire"){
-    if (choice2 === "earth"){
-      alert ("fire wins!");
+//player 1 and 2 selections
+function selection(){
+window.addEventListener("keydown", selection1);
+window.addEventListener("keydown", selection2);
+  function selection1(e) {
+    var p1img = document.getElementById("p1img");
+    var play = e.keyCode;
+    switch (play) {
+      case 81: p1img.innerHTML = "fire"; p1Choice = "fire";   window.removeEventListener("keydown", selection1); break;
+      case 87: p1img.innerHTML = "water"; p1Choice = "water";   window.removeEventListener("keydown", selection1); break;
+      case 69: p1img.innerHTML = "earth"; p1Choice = "earth";   window.removeEventListener("keydown", selection1); break;
+      default: break;
     }
-    else{
-      alert ("water wins!");
-    }
-  }
-
-  else if (choice1 === "paper"){
-    if (choice2 === "rock"){
-      alert ("Paper wins!");
-    }
-    else{
-      confirm ("Scissors wins!");
+    e.stopPropagation();
+    console.log(p1Choice);
+    if ((p1Choice === "fire" || p1Choice === "water" || p1Choice === "earth") && (p2Choice === "fire" || p2Choice === "water" || p2Choice === "earth") ) {
+      checkWin();
     }
   }
 
-  else if (choice1 === "scissors"){
-    if (choice2 === "paper"){
-      confirm ("Scissors wins!");
+  function selection2(e) {
+    var p2img = document.getElementById("p2img");
+    var play = e.keyCode;
+    switch (play) {
+      case 73: p2img.innerHTML = "fire"; p2Choice = "fire"; window.removeEventListener("keydown", selection2); break;
+      case 79: p2img.innerHTML = "water"; p2Choice = "water"; window.removeEventListener("keydown", selection2); break;
+      case 80: p2img.innerHTML = "earth"; p2Choice = "earth";window.removeEventListener("keydown", selection2); break;
+      default: break;
     }
-    else{
-      confirm ("Rock wins!");
+    e.stopPropagation();
+    console.log(p2Choice);
+    if ((p1Choice === "fire" || p1Choice === "water" || p1Choice === "earth") && (p2Choice === "fire" || p2Choice === "water" || p2Choice === "earth") ) {
+      checkWin();
+    }
+
+  }
+}
+
+
+// determine winner
+  function checkWin() {
+    if (p1Choice === p2Choice) {
+      console.log("The result is a tie!");
+      clear();
+    }
+    else if (p1Choice === "fire" && p2Choice === "earth") {
+        console.log("fire wins!");
+        p2HP--;
+        console.log(`player 2 health: ${p2HP}`);
+        clear();
+    }
+    else if (p1Choice === "water" && p2Choice === "fire") {
+        console.log("water wins!");
+        p2HP--;
+        console.log(`player 2 health: ${p2HP}`);
+        clear();
+    }
+    else if (p1Choice === "earth" && p2Choice === "water") {
+        console.log("earth wins!");
+        p2HP--;
+        console.log(`player 2 health: ${p2HP}`);
+        clear();
+    }
+    else if (p2Choice === "fire" && p1Choice === "earth") {
+        console.log("fire wins!");
+        p1HP--;
+        console.log(`player 1 health: ${p1HP}`);
+        clear();
+    }
+    else if (p2Choice === "water" && p1Choice === "fire") {
+        console.log("water wins!");
+        p1HP--;
+        console.log(`player 1 health: ${p1HP}`);
+        clear();
+    }
+    else if (p2Choice === "earth" && p1Choice === "water") {
+        console.log("earth wins!");
+        p1HP--;
+        console.log(`player 1 health: ${p1HP}`);
+        clear();
     }
   }
 
-  };
-winner(p1Element, p2Element)
+
+//clear selections
+  function clear() {
+    var p1img = document.getElementById("p1img");
+    var p2img = document.getElementById("p2img");
+    p1Choice = "";
+    p2Choice = "";
+    p1img.innerHTML="";
+    p2img.innerHTML="";
+    if (p1HP > 0 && p2HP >0) {
+    start();
+    }
+    else if (p1HP == 0) {
+      alert("Player 2 WINS!");
+    }
+    else if (p2HP == 0) {
+      alert("Player 1 WINS!")
+    }
+  }
+  
+// reset health
+// function rematch() {
+//   var rematchButton = querySelector("input");
+//   rematchButton.addEventListener("onclick", function() {
+//     window.location.reload();
+//   })
+}
